d94b488ddf5082bae6100771c06afd497410b649
zeynep <49367777+zeinab-khj@users.noreply.github.com>
2026-09-11 00:29:17 +0330

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,15 +9,15 @@ def recall_at_k(retrieved_ids, relevant_ids, k=10):
 
         query_id = queries_df.iloc[query_idx]["_id"]
         total_relevant = len(qrels_dict[query_id])
-    
+
         # -------------------------
         # Recall@10
         # -------------------------
         retrieved_relevant = sum(labels)
-    
+
         recall = retrieved_relevant / total_relevant
         recall_scores.append(recall)
-      return recall_scores
+    return recall_scores
 
 
 def mrr_at_k(retrieved_ids, relevant_ids, k=10):
@@ -26,38 +26,39 @@ def mrr_at_k(retrieved_ids, relevant_ids, k=10):
 
         query_id = queries_df.iloc[query_idx]["_id"]
         total_relevant = len(qrels_dict[query_id])
+        reciprocal_rank = 0
         for rank, label in enumerate(labels, start=1):
             if label == 1:
                 reciprocal_rank = 1 / rank
                 break
-    
-        mrr_scores.append(reciprocal_rank)   
-      return mrr_scores
+
+        mrr_scores.append(reciprocal_rank)
+    return mrr_scores
 
 
 def ndcg_at_k(retrieved_ids, relevant_ids, k=10):
-     ndcg_scores = []
-     for query_idx, labels in enumerate(fine_tuned_retrieval_labels):
+    ndcg_scores = []
+    for query_idx, labels in enumerate(fine_tuned_retrieval_labels):
 
         query_id = queries_df.iloc[query_idx]["_id"]
         total_relevant = len(qrels_dict[query_id])
 
-         dcg = 0.0
+        dcg = 0.0
 
-          for rank, label in enumerate(labels, start=1):
-              if label == 1:
-                  dcg += 1 / math.log2(rank + 1)
-      
-          ideal_relevant = min(total_relevant, len(labels))
-      
-          idcg = 0.0
-      
-          for rank in range(1, ideal_relevant + 1):
-              idcg += 1 / math.log2(rank + 1)
-      
-          ndcg = dcg / idcg if idcg > 0 else 0.0
-          ndcg_scores.append(ndcg)
-       return ndcg_scores
+        for rank, label in enumerate(labels, start=1):
+            if label == 1:
+                dcg += 1 / math.log2(rank + 1)
+
+        ideal_relevant = min(total_relevant, len(labels))
+
+        idcg = 0.0
+
+        for rank in range(1, ideal_relevant + 1):
+            idcg += 1 / math.log2(rank + 1)
+
+        ndcg = dcg / idcg if idcg > 0 else 0.0
+        ndcg_scores.append(ndcg)
+    return ndcg_scores
 
 
 
